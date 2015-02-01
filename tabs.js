f2bd24e8c243b76ce84fd36f22145bedd1827496
harmon758 <harmon758@gmail.com>
2015-02-01 04:00:45 -0600

    Fonts + Tab Switch Fade

--- tabs.js
+++ tabs.js
@@ -2,8 +2,8 @@ jQuery(document).ready(function() {
     jQuery('.tabs .tab-links a').on('click', function(e)  {
         var currentAttrValue = jQuery(this).attr('href');
  
-        // Show/Hide Tabs
-        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+		// Show/Hide Tabs
+		jQuery('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
  
         // Change/remove current tab to active
         jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
