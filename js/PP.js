871d7856cc017222185dad599ac86c169a3496df
harmon758 <harmon758@gmail.com>
2015-04-26 16:15:09 -0500

    Local Storage

--- js/PP.js
+++ js/PP.js
@@ -33,6 +33,38 @@ function Recalculate_XP(){
 	if(document.getElementById("Meilyr_VOS_Checkbox").checked){ Meilyr_XP.innerHTML = (Math.round(Meilyr_XP.innerHTML * 12) / 10).toFixed(1); VOS_Checked++; }
 	if(VOS_Checked > 2){ document.getElementById("VOS_Notification").innerHTML = "You have more than 2 VOS's checked"; }
 	else{ document.getElementById("VOS_Notification").innerHTML = ""; }
+	Save_Session();
+}
+
+function Save_Session(){
+	localStorage.setItem("Ibis_Pieces", document.getElementById("Ibis_Pieces").value);
+	localStorage.setItem("Clan_Avatar", document.getElementById("Clan_Avatar").value);
+	localStorage.setItem("Iorwerth_VOS_Checkbox", document.getElementById("Iorwerth_VOS_Checkbox").checked);
+	localStorage.setItem("Ithell_VOS_Checkbox", document.getElementById("Ithell_VOS_Checkbox").checked);
+	localStorage.setItem("Cadarn_VOS_Checkbox", document.getElementById("Cadarn_VOS_Checkbox").checked);
+	localStorage.setItem("Amlodd_VOS_Checkbox", document.getElementById("Amlodd_VOS_Checkbox").checked);
+	localStorage.setItem("Trahaearn_VOS_Checkbox", document.getElementById("Trahaearn_VOS_Checkbox").checked);
+	localStorage.setItem("Hefin_VOS_Checkbox", document.getElementById("Hefin_VOS_Checkbox").checked);
+	localStorage.setItem("Crwys_VOS_Checkbox", document.getElementById("Crwys_VOS_Checkbox").checked);
+	localStorage.setItem("Meilyr_VOS_Checkbox", document.getElementById("Meilyr_VOS_Checkbox").checked);
+	localStorage.setItem("VOS_Notification", document.getElementById("VOS_Notification").innerHTML);
+	localStorage.setItem("Iorwerth_XP", document.getElementById("Iorwerth_XP").innerHTML);
+	localStorage.setItem("Ithell_XP", document.getElementById("Ithell_XP").innerHTML);
+	localStorage.setItem("Cadarn_XP", document.getElementById("Cadarn_XP").innerHTML);
+	localStorage.setItem("Amlodd_XP", document.getElementById("Amlodd_XP").innerHTML);
+	localStorage.setItem("Trahaearn_XP", document.getElementById("Trahaearn_XP").innerHTML);
+	localStorage.setItem("Hefin_XP", document.getElementById("Hefin_XP").innerHTML);
+	localStorage.setItem("Crwys_XP", document.getElementById("Crwys_XP").innerHTML);
+	localStorage.setItem("Meilyr_XP", document.getElementById("Meilyr_XP").innerHTML);
+	localStorage.setItem("Uncheck_Noticed", document.getElementById("Uncheck_Noticed").checked);
+	localStorage.setItem("Iorwerth_Noticed_Checkbox", document.getElementById("Iorwerth_Noticed_Checkbox").checked);
+	localStorage.setItem("Ithell_Noticed_Checkbox", document.getElementById("Ithell_Noticed_Checkbox").checked);
+	localStorage.setItem("Cadarn_Noticed_Checkbox", document.getElementById("Cadarn_Noticed_Checkbox").checked);
+	localStorage.setItem("Amlodd_Noticed_Checkbox", document.getElementById("Amlodd_Noticed_Checkbox").checked);
+	localStorage.setItem("Trahaearn_Noticed_Checkbox", document.getElementById("Trahaearn_Noticed_Checkbox").checked);
+	localStorage.setItem("Hefin_Noticed_Checkbox", document.getElementById("Hefin_Noticed_Checkbox").checked);
+	localStorage.setItem("Crwys_Noticed_Checkbox", document.getElementById("Crwys_Noticed_Checkbox").checked);
+	localStorage.setItem("Meilyr_Noticed_Checkbox", document.getElementById("Meilyr_Noticed_Checkbox").checked);
 }
 
 function Clear_VOS(){
@@ -64,6 +96,7 @@ function Clear_Noticed(){
 	Update_Noticed_Timer(document.getElementById("Hefin_Noticed_Checkbox"));
 	Update_Noticed_Timer(document.getElementById("Crwys_Noticed_Checkbox"));
 	Update_Noticed_Timer(document.getElementById("Meilyr_Noticed_Checkbox"));
+	Save_Session();
 }
 
 function Clear_Notifications(){
@@ -78,4 +111,36 @@ function Update_Noticed_Timer(Clan){
 		document.getElementById(Clan.value + "_Noticed_Timer").innerHTML = "00:00";
 		document.getElementById(Clan.value + "_Noticed_Timer").style.color = "green";
 	}
+	Save_Session();
+}
+
+function Load_Saved(){
+	if(localStorage.getItem("Ibis_Pieces") != null){ document.getElementById("Ibis_Pieces").value = localStorage.getItem("Ibis_Pieces"); }
+	if(localStorage.getItem("Clan_Avatar") != null){ document.getElementById("Clan_Avatar").value = localStorage.getItem("Clan_Avatar"); }
+	if(localStorage.getItem("Iorwerth_VOS_Checkbox") != null){ document.getElementById("Iorwerth_VOS_Checkbox").checked = (localStorage.getItem("Iorwerth_VOS_Checkbox") === "true"); }
+	if(localStorage.getItem("Ithell_VOS_Checkbox") != null){ document.getElementById("Ithell_VOS_Checkbox").checked = (localStorage.getItem("Ithell_VOS_Checkbox") === "true"); }
+	if(localStorage.getItem("Cadarn_VOS_Checkbox") != null){ document.getElementById("Cadarn_VOS_Checkbox").checked = (localStorage.getItem("Cadarn_VOS_Checkbox") === "true"); }
+	if(localStorage.getItem("Amlodd_VOS_Checkbox") != null){ document.getElementById("Amlodd_VOS_Checkbox").checked = (localStorage.getItem("Amlodd_VOS_Checkbox") === "true"); }
+	if(localStorage.getItem("Trahaearn_VOS_Checkbox") != null){ document.getElementById("Trahaearn_VOS_Checkbox").checked = (localStorage.getItem("Trahaearn_VOS_Checkbox") === "true"); }
+	if(localStorage.getItem("Hefin_VOS_Checkbox") != null){ document.getElementById("Hefin_VOS_Checkbox").checked = (localStorage.getItem("Hefin_VOS_Checkbox") === "true"); }
+	if(localStorage.getItem("Crwys_VOS_Checkbox") != null){ document.getElementById("Crwys_VOS_Checkbox").checked = (localStorage.getItem("Crwys_VOS_Checkbox") === "true"); }
+	if(localStorage.getItem("Meilyr_VOS_Checkbox") != null){ document.getElementById("Meilyr_VOS_Checkbox").checked = (localStorage.getItem("Meilyr_VOS_Checkbox") === "true"); }
+	if(localStorage.getItem("VOS_Notification") != null){ document.getElementById("VOS_Notification").innerHTML = localStorage.getItem("VOS_Notification"); }
+	if(localStorage.getItem("Iorwerth_XP") != null){ document.getElementById("Iorwerth_XP").innerHTML = localStorage.getItem("Iorwerth_XP"); }
+	if(localStorage.getItem("Ithell_XP") != null){ document.getElementById("Ithell_XP").innerHTML = localStorage.getItem("Ithell_XP"); }
+	if(localStorage.getItem("Cadarn_XP") != null){ document.getElementById("Cadarn_XP").innerHTML = localStorage.getItem("Cadarn_XP"); }
+	if(localStorage.getItem("Amlodd_XP") != null){ document.getElementById("Amlodd_XP").innerHTML = localStorage.getItem("Amlodd_XP"); }
+	if(localStorage.getItem("Trahaearn_XP") != null){ document.getElementById("Trahaearn_XP").innerHTML = localStorage.getItem("Trahaearn_XP"); }
+	if(localStorage.getItem("Hefin_XP") != null){ document.getElementById("Hefin_XP").innerHTML = localStorage.getItem("Hefin_XP"); }
+	if(localStorage.getItem("Crwys_XP") != null){ document.getElementById("Crwys_XP").innerHTML = localStorage.getItem("Crwys_XP"); }
+	if(localStorage.getItem("Meilyr_XP") != null){ document.getElementById("Meilyr_XP").innerHTML = localStorage.getItem("Meilyr_XP"); }
+	if(localStorage.getItem("Uncheck_Noticed") != null){ document.getElementById("Uncheck_Noticed").checked = (localStorage.getItem("Uncheck_Noticed") === "true"); }
+	if(localStorage.getItem("Iorwerth_Noticed_Checkbox") != null){ document.getElementById("Iorwerth_Noticed_Checkbox").checked = (localStorage.getItem("Iorwerth_Noticed_Checkbox") === "true"); }
+	if(localStorage.getItem("Ithell_Noticed_Checkbox") != null){ document.getElementById("Ithell_Noticed_Checkbox").checked = (localStorage.getItem("Ithell_Noticed_Checkbox") === "true"); }
+	if(localStorage.getItem("Cadarn_Noticed_Checkbox") != null){ document.getElementById("Cadarn_Noticed_Checkbox").checked = (localStorage.getItem("Cadarn_Noticed_Checkbox") === "true"); }
+	if(localStorage.getItem("Amlodd_Noticed_Checkbox") != null){ document.getElementById("Amlodd_Noticed_Checkbox").checked = (localStorage.getItem("Amlodd_Noticed_Checkbox") === "true"); }
+	if(localStorage.getItem("Trahaearn_Noticed_Checkbox") != null){ document.getElementById("Trahaearn_Noticed_Checkbox").checked = (localStorage.getItem("Trahaearn_Noticed_Checkbox") === "true"); }
+	if(localStorage.getItem("Hefin_Noticed_Checkbox") != null){ document.getElementById("Hefin_Noticed_Checkbox").checked = (localStorage.getItem("Hefin_Noticed_Checkbox") === "true"); }
+	if(localStorage.getItem("Crwys_Noticed_Checkbox") != null){ document.getElementById("Crwys_Noticed_Checkbox").checked = (localStorage.getItem("Crwys_Noticed_Checkbox") === "true"); }
+	if(localStorage.getItem("Meilyr_Noticed_Checkbox") != null){ document.getElementById("Meilyr_Noticed_Checkbox").checked = (localStorage.getItem("Meilyr_Noticed_Checkbox") === "true"); }
 }
